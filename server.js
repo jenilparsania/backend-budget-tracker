0c44a4ac2d287125eda7d5e8346dfbed3e073f63
jenilparsania <151541448+jenilparsania@users.noreply.github.com>
2025-06-17 23:31:51 -0300

    solved the cors issue

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 
 const rootRouter = require("./routes/index");
 const { mongoURL } = require("./config/config");
 
+// Middleware
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Accept'],
+}));
 app.use(express.json());
+
+// Routes
 app.use("/", rootRouter);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Something went wrong!' });
+});
 
 async function main() {
     try {
@@ -24,3 +39,6 @@ async function main() {
 }
 
 main();
+
+// update request to update the transaction is still remaining
+
